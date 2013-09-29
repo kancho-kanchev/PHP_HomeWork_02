7eae1a322d7167ec2151d85f408f6ebd75df7a9a
K. Kanchev <no thank you>
2013-09-29 22:58:29 +0300

    init yet

--- files.php
+++ files.php
@@ -1,1 +1,5 @@
 <?php
+session_start();
+mb_internal_encoding('UTF-8');
+$pageTitle = 'Логин';
+include 'includes/header.php';
--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <?php
+session_start();
+mb_internal_encoding('UTF-8');
+$pageTitle = 'Логин';
+include 'includes/header.php';
+?>
+	<form method="POST">
+	<div>име:<input type="text" name="username" /></div>
+	<div>телефон:<input type="text" name="phone" /></div>
+	<div>
+		<select name="group">
+			<?php 
+				foreach ($groups as $key=>$value) {
+					echo'<option value="'.$key.'">'.$value.'</option>';
+				}
+			?>
+		</select>
+	</div>
+	<div><input type="submit" value="Добави" /></div>
+	</form>
+<?php 
+include 'includes/footer.php';
