0e28f5aedd664e83f64e68b50981f0b307a2a492
K. Kanchev <no thank you>
2013-09-30 21:17:49 +0300

    make upload form

--- files.php
+++ files.php
@@ -7,6 +7,10 @@ if (isset($_SESSION['isLogged'])) {
 	//$files = scandir($directory)
 	if ($_POST) {
 		if (count($_FILES>0)) {
+			echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+			echo "Type: " . $_FILES["file"]["type"] . "<br>";
+			echo "Size: " . (round($_FILES["file"]["size"] / 1024, 2)) . " kB<br>";
+			echo "Stored in: " . $_FILES["file"]["tmp_name"] . "<br>";
 			if (move_uploaded_file($_FILES['file']['tmp_name'], $username.DIRECTORY_SEPARATOR.$_FILES['file']['name'])) {
 				echo 'Файлът е качен успешно в системата';
 			}
@@ -16,9 +20,9 @@ if (isset($_SESSION['isLogged'])) {
 		}
 	}
 ?>
-	<form method="POST" enctype="multipart/form-data">
+	<form method="POST" action="files.php" enctype="multipart/form-data">
 		<div>Качи файл: <input type="file" name="file" /></div>
-		<div><input type="submit" value="Качи файла" /></div>
+		<div><input type="submit" name="submit" value="Качи файла" /></div>
 	</form>
 	<div>
 		<a href="destroy.php">Изход</a>
